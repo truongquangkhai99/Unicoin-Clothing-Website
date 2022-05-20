e3290f87435224595ffd7e1813d6cd5bb24421bb
quangvm35 <quangvmph12936@fpt.edu.vn>
2022-05-20 11:31:33 +0700

    Tạo ResponPage

---
+++ customer-service/src/main/java/com/unicoin/customer/common/RestResponsePage.java
@@ -0,0 +1,40 @@
+package com.unicoin.customer.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+public class RestResponsePage<T> {
+    private Pagination pagination;
+    private List<T> content;
+
+    public RestResponsePage(List<T> content, int page, int size, int numberOfElements, int totalPages) {
+        this.content = content;
+        Pagination pagination = new Pagination();
+        pagination.currentPage = page;
+        pagination.size = size;
+        pagination.totalPages = totalPages;
+        pagination.numberOfElements = numberOfElements;
+        this.pagination = pagination;
+    }
+
+    public RestResponsePage(List<T> content, int page, int size, long numberOfElements, int totalPages) {
+        this.content = content;
+        Pagination pagination = new Pagination();
+        pagination.currentPage = page;
+        pagination.size = size;
+        pagination.totalPages = totalPages;
+        pagination.numberOfElements = numberOfElements;
+        this.pagination = pagination;
+    }
+
+    @Getter
+    @Setter
+    public class Pagination {
+        private int currentPage;
+        private int size;
+        private int totalPages;
+        private long numberOfElements;
+    }
+}
