c48c7a41db22fef3bc5b94fe29a70e3342e95ac4
haile <hai95.lv@gmail.com>
2022-06-27 08:59:49 +0700

    edit select variant

--- product-service/src/main/java/com/unicoin/product/repository/VariantRepository.java
+++ product-service/src/main/java/com/unicoin/product/repository/VariantRepository.java
@@ -4,6 +4,8 @@ import com.unicoin.product.entity.Product;
 import com.unicoin.product.entity.Variant;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +14,8 @@ import java.util.List;
 public interface VariantRepository extends JpaRepository<Variant, Long> {
     List<Variant> findAllByProduct(Product product);
 
-    List<Variant> findAllByProduct(Product product, Sort sort);
+    @Query("select v from Variant v where v.product = :product and v.qty > 0")
+    List<Variant> findAllByProduct(@Param("product")Product product, Sort sort);
 
    boolean existsByProductAndStatus(Product product, Integer status);
 }
